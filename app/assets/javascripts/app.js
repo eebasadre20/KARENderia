70478bd568bbb46e798c8416d3b9c4c6f18e4dcb
eebasadre20 <eebasadre20@gmail.com>
2016-07-27 22:43:23 +0800

    ( #8 ) Initial store to handle the comments and flux dispatcher
    * Initial store for comments
    * Add flux dispatcher in adding comments

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,4 +3,34 @@ var Constants = {
   ADD_COMMENT: 'comments.add'
 }
 
+var Store = new _.extend( {}, EventEmitter.prototype, {
+  _comments: [],
+  addComment: function( comment ) {
+    this._comments[comment.id] = comment;
+  },
+  comments: function( ) {
+    return this._comments;
+  },
+  addChangeListner: function ( callback ) {
+    this.on( Constants.CHANGE_EVENT, callback );
+  },
+  removeChangeListner: function ( callback ) {
+    this.removeListner( Constants.CHANGE_EVENT, callback );
+  },
+  emitChange: function ( ) {
+    this.emit( Constants.CHANGE_EVENT );
+  }
+});
 
+var AppDispatcher = new FluxDispatcher();
+
+AppDispatcher.register( function ( payload ) {
+  var action = payload.actionType;
+  switch( action ) {
+    case Constants.ADD_COMMENT:
+      Store.addComment( payload.comment );
+      break;
+    default:
+      // NO-OPERATION
+  }
+} );
