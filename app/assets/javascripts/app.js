ec0aeebc76fc719fa704340cf81821cdf9fce97e
eebasadre <edsil@piktochart.com>
2016-07-27 22:36:07 +0800

    Initial store for comments

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,4 +3,21 @@ var Constants = {
   ADD_COMMENT: 'comments.add'
 }
 
-
+var Store = new _.extend( {}, EventEmitter.prototype, {
+  _comments: [],
+  addComment: function( comment ) {
+    this._comments[comment.id] = comment;
+  },
+  comments: function( ) {
+    return this._comments;
+  },
+  addChangeListner: function ( callback ) {
+    this.on( Constants.CHANGE_EVENT, callback );
+  },
+  removeChangeListner: function ( callback ) {
+    this.removeListner( Constants.CHANGE_EVENT, callback );
+  },
+  emitChange: function ( ) {
+    this.emit( Constants.CHANGE_EVENT );
+  }
+}); 
