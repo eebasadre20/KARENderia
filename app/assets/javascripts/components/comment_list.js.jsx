8f1cdb801b6cd9648f215bb83394d5d283b2a738
eebasadre20 <eebasadre20@gmail.com>
2016-07-29 23:50:32 +0800

    ( #12 ) Update the app.js to fully es6.

--- app/assets/javascripts/components/comment_list.js.jsx
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -1,15 +1,15 @@
 var CommentList = React.createClass( {
   componentDidMount: function() {
-    Store.addChangeListner( this._onChange);
+    commentStore.addChangeListner( this._onChange);
   },
   componentWillUnmount: function() {
-    Store.removeChangeListner( this._onChange );
+    commentStore.removeChangeListner( this._onChange );
   },
   render: function( ) {
     console.log( 'rendering...');
     return (
       <div>
-        { Store.comments().map( function( comment ) {
+        { commentStore.comments().map( function( comment ) {
           // we can use this way {... comment} to remove the rendundancy
           // in stating the comment attributes but we just live it like
           // this so that it makes more clear for the user.
